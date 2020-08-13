2e5e34c154e711f449c80845a477af5101ecab22
aydevelop <ay.develop1@gmail.com>
2020-08-13 21:29:19 +0300

    B. Posting user to database

--- app.js
+++ app.js
@@ -4,14 +4,22 @@ const mongoose = require('mongoose')
 const { MONGOURI } = require('./keys')
 const PORT = 5000
 
+//-------------------------------------------------------
 mongoose.connect(MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
-
 db.on('open', () => {
   app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
   })
 })
 
+//-------------------------------------------------------
 app.use(express.json())
 app.use(require('./routes/auth'))
+
+app.use(function (err, req, res, next) {
+  res.status(500).json({
+    success: false,
+    message: err.message,
+  })
+})
--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,34 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
+const t = require('../init/try-catch')
 
 router.get('/', (req, res) => {
   res.send('hello')
 })
 
-router.post('/signup', (req, res) => {
-  const { name, email, password } = req.body
-  if (!email || !password || !name) {
-    return res.status(422).json({ error: 'not all fields' })
-  }
+router.post(
+  '/signup',
+  t(async (req, res) => {
+    const { name, email, password } = req.body
+    if (!email || !password || !name) {
+      return res.status(422).json({ error: 'not all fields' })
+    }
 
-  console.log('LOGG')
-  res.json({ message: 'successfuly posted' })
-})
+    let check = await User.findOne({ email: email })
+    if (check) {
+      return res.status(422).send('user exists')
+    }
+
+    let user = new User({
+      name,
+      email,
+      password,
+    })
+
+    let result = await user.save()
+    res.send('user saved')
+  })
+)
 
 module.exports = router
