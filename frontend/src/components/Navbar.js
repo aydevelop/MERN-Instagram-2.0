787d7d785271fac0812acd7c12d35817ee859864
aydevelop <ay.develop1@gmail.com>
2020-08-19 13:38:22 +0300

    F. Search users client side

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,30 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef, useEffect, useState } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import { UserContext } from '../App'
+import M from 'materialize-css'
 
 const NavBar = () => {
+  const modal = useRef(null)
   const { state, dispatch } = useContext(UserContext)
   const history = useHistory()
+  const [search, setSearch] = useState('tmp')
+
+  useEffect(() => {
+    M.Modal.init(modal.current)
+    console.log('init....')
+  }, [])
 
   const renderList = () => {
     if (localStorage.getItem('user')) {
       return (
         <React.Fragment>
+          <li>
+            <a data-target='modal1' className='modal-trigger'>
+              <i style={{ marginTop: '2px' }} className='large material-icons'>
+                search
+              </i>
+            </a>
+          </li>
           <li>
             <Link to='/profile'>Profile</Link>
           </li>
@@ -44,16 +59,57 @@ const NavBar = () => {
   }
 
   return (
-    <nav>
-      <div className='nav-wrapper'>
-        <Link to='/' style={{ marginLeft: '20px' }} className='brand-logo left'>
-          Instagram &nbsp;2.0
-        </Link>
-        <ul id='nav-mobile' className='right'>
-          {renderList()}
-        </ul>
+    <React.Fragment>
+      <nav>
+        <div className='nav-wrapper'>
+          <Link
+            to='/'
+            style={{ marginLeft: '20px' }}
+            className='brand-logo left'
+          >
+            Instagram &nbsp;2.0
+          </Link>
+          <ul id='nav-mobile' className='right'>
+            {renderList()}
+          </ul>
+        </div>
+      </nav>
+      <div id='modal1' className='modal' ref={modal}>
+        <div className='modal-content' style={{ color: 'black' }}>
+          <input
+            onChange={(e) => setSearch(e.target.value)}
+            type='text'
+            placeholder='search users'
+            value={search}
+          ></input>
+          <ul
+            className='collection'
+            style={{ display: 'flex', flexDirection: 'column' }}
+          >
+            <li className='collection-item'>Alvin</li>
+            <li className='collection-item'>Alvin</li>
+            <li className='collection-item'>Alvin</li>
+            <li className='collection-item'>Alvin</li>
+            <li className='collection-item'>Alvin</li>
+            <li className='collection-item'>Alvin</li>
+            <li className='collection-item'>Alvin</li>
+            <li className='collection-item'>Alvin</li>
+            <li className='collection-item'>Alvin</li>
+            <li className='collection-item'>Alvin</li>
+            <li className='collection-item'>Alvin</li>
+            <li className='collection-item'>Alvin</li>
+          </ul>
+        </div>
+        <div className='modal-footer'>
+          <a
+            href='#!'
+            className='modal-close waves-effect waves-green btn-flat'
+          >
+            Agree
+          </a>
+        </div>
       </div>
-    </nav>
+    </React.Fragment>
   )
 }
 
